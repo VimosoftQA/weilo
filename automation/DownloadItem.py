67f0c0bf3e75b6906e96e61eddfeaacb6d26be15
Carly-vimo <carly@vimosoft.com>
2025-02-19 14:02:15 +0900

    feat : 다운로드 아이콘이 있으면 해당 에셋 다운로드

--- automation/DownloadItem.py
+++ automation/DownloadItem.py
@@ -6,24 +6,22 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(verbose=True)
+action = Action()
+isConnection = checkNetwork()
+driver.activate_app(os.getenv("APP_NAME"))
 
 def download_item():
 
-    action = Action()
-    isConnection = checkNetwork()
-
-    driver.activate_app(os.getenv("APP_NAME"))
-    action.click("inspector_download_ic")
-
     if isConnection:
+        action.click("inspector_download_ic")
         logger.info("Success to download item")
-
+        return True
     else:
         time.sleep(1)
         action.find("네트워크 연결 상태를 확인하세요")
         logger.info("Failed to download item")
-
+        return False
 
 if __name__ == '__main__':
-    download_item()
+    result = download_item()
 
