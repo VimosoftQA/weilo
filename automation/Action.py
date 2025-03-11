2a4f798c27fd81155affdb5bde59c18b1524bd8b
carly-vimo <carly@vimosoft.com>
2025-03-11 10:43:32 +0900

    feat : 필요한 기능 추가 및 코드 수정

--- automation/Action.py
+++ automation/Action.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.keys import Keys
 from automation import driver
 import time
 
-
 class Action:
     """
     자동화 동작에 필요한 기능들을 정의한 클래스
@@ -16,7 +15,6 @@ class Action:
 
     def click(self,value):
         driver.find_element(by = AppiumBy.ACCESSIBILITY_ID , value = value).click()
-        return value
 
     def click_xpath(self,value):
         driver.find_element(by = AppiumBy.XPATH , value = value).click()
@@ -30,6 +28,11 @@ class Action:
     def click_ios_class_chain(self,value):
         driver.find_element(by = AppiumBy.IOS_CLASS_CHAIN, value = value).click()
         return value
+
+    def click_coordinate(self,value : dict):
+        X = value.get("x")
+        Y = value.get("y")
+        driver.tap([(X,Y)])
 
     def find(self,value):
         element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=value)
@@ -68,31 +71,15 @@ class Action:
     def swipe(self,sx, sy, ex, ey):
         driver.swipe(start_x=sx, start_y=sy, end_x= ex, end_y= ey)
 
-    def tap_coordinate(self, X,Y,sec) : 
-        driver.tap([(X,Y)],sec)
-
     def screenshot(self,route):
         timestamp = time.strftime("%Y%m%d_%H%M%S")
         screenshot_name = f"screenshot_{route}_{timestamp}"
         driver.save_screenshot(f'screenshot/{screenshot_name}.png')
 
+    def find_element_coordinate(self,value) -> dict:
+        element_location = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=value).location
+        return element_location # location이 dictionary 의 형태로 반환됨
 
-    # def scroll_by_ios_predicate(driver, text):
-    #     """
-    #     iOS Predicate String을 사용한 스크롤 (iOS 전용)
-    #     :param driver: Appium 드라이버
-    #     :param text: 찾고자 하는 텍스트
-    #     :return: 찾은 요소 또는 None
-    #     """
-    #     try:
-    #         return driver.find_element(AppiumBy.ACCESSIBILITY_ID,
-    #                                    f'label == "{text}" OR name == "{text}" OR value == "{text}"')
-    #     except:
-    #         return None
-
-
-
-    
 
 
 
