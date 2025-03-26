8f6693c0dbd433b7e3d8492835b0b0bdd6a8c9d8
carly-vimo <carly@vimosoft.com>
2025-03-26 17:41:20 +0900

    feat : Transition 브라우저 코드 수정 완료

--- browser/Transition.py
+++ browser/Transition.py
@@ -13,48 +13,62 @@ class Transition:
     def __init__(self):
         self.action = Action()
         self.source_player = SourcePlayer()
+        self.all_categories = {
+            "package_0" : "NEW",
+            "package_1" : "기본",
+            "package_2" : "모션",
+            "package_3" : "회전",
+            "package_4" : "슬라이드",
+            "package_5" : "글리치",
+            "package_6" : "빛",
+            "package_7" : "블러",
+            "package_8" : "마스크",
+            "package_9" : "그래픽"
+        }
 
     def open_transition_browser(self):
         self.action.click("browser_transition_button")
         logger.info("[Browser > Transition] open Transition Browser")
 
     def tap_Transition_category(self):
-        all_categories = [["browser_new_ic","기본","모션","회전","슬라이드", "글리치"],
-                          ["빛","블러","마스크","그래픽"]]
 
         self.action.swipe(58, 134, 419, 134) #TODO 좌표를 쓰지 않고 할 수 있는 방법 찾아보기
         logger.info("[Browser > Transition] initialize Transition Sub Category")
 
-        for categories in all_categories:
-            logger.info("[Browser > Transition] tap Transition Sub Category")
-            for category in categories:
-                self.action.click(category)
-                if category == "browser_new_ic":
-                    category = "NEW"
-                logger.info(f"[Browser > Transition] tap Trnasition Sub Category : {category}")
+        for key, value in self.all_categories.items():
+            while True:
+                try:
+                    if self.action.find(key):
+                        self.action.click(key)
+                        logger.info(f"[Browser > Transition] tap Transition Sub Category : {value}")
+                    self.action.swipe(419, 134, 350, 134)
+                    time.sleep(1)
+                    break
+                except:
+                    self.action.swipe(419, 134, 350, 134)
+                    time.sleep(1)
 
-            self.action.swipe(419, 134, 288, 134)
-            time.sleep(1)
 
     def random_find_category_asset(self):
-        transition_categories = ["browser_new_ic","기본","모션","회전","슬라이드", "글리치","빛","블러","마스크","그래픽"]
-        random_category = choice_random(transition_categories) #카테고리 랜덤 선택
-        find_category(random_category)
-        idx = transition_categories.index(random_category) + 1
-        self.action.click(f"asset_{idx}_1")
-        logger.info(f"[Browser > Text] choose {random_category} asset 1")
+        random_category = choice_random(self.all_categories) #카테고리 랜덤 선택 (카테고리 명, 카테고리 package 명)
+        if find_category(random_category[1]) == 1 :
+            idx = 1
+            self.action.click(f"asset_{idx}_1")
+            logger.info(f"[Browser > Transition ] choose NEW asset 1")
+        else:
+            idx = int(random_category[1][-1]) +1
+            self.action.click(f"asset_{idx}_1")
+            logger.info(f"[Broswer > Transition ]  choose {random_category[0]} asset 1")
         time.sleep(1)
 
-    def play_source_player(self,coordinate):
-        self.action.click_coordinate(coordinate)
-        logger.info("[Browser > Transition] play source player")
+    def play_source_player(self):
         sourcePlayer = SourcePlayer()
-        time.sleep(1)
+        logger.info("[Browser > Transition] play source player")
         sourcePlayer.tap_next_frame_button()
         sourcePlayer.tap_prev_frame_button()
         sourcePlayer.tap_play_button()
         sourcePlayer.tap_reset_button()
-        sourcePlayer.tap_play_button()
+        sourcePlayer.tap_add_button()
         sourcePlayer.tap_screenshot()
 
 
