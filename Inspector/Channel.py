ff030c1c065556cae8a5fdb9e439ba65352fa796
carly-vimo <carly@vimosoft.com>
2025-05-13 16:43:27 +0900

    feat : Channel 완료

---
+++ Inspector/Channel.py
@@ -0,0 +1,56 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+import Inspector
+import time
+
+class ChannelAction:
+    def __init__(self):
+        self.driver = Inspector.initialize_driver()
+        self.action = ActionChains(self.driver)
+        self.logger = Inspector.setup_logger()
+
+    def tap_element(self, id):
+        self.driver.find_element(by=By.ID, value=id).click()
+
+    def open_drop_down(self, id):
+        self.driver.find_element(by=By.ID, value=id).click()
+
+class Channel:
+    def __init__(self):
+        self.channel_action = ChannelAction()
+
+    def open_channel_inspector(self):
+        self.channel_action.tap_element("inspector_channel")
+        self.channel_action.logger.info("[Channel] Open Channel Inspector")
+
+    def open_channel_dropdown(self):
+        self.channel_action.tap_element("channel_channel_dropdown")
+
+    # 채널 옵션 선택하기 : "오른쪽_소리를_채우기" 와 같이 언더바로 연결한다.
+    def select_channel_option(self, option = "stereo"):
+        self.channel_action.tap_element("dropdown_item_" + option)
+        self.channel_action.logger.info("[Channel] Select Channel Option : " + option)
+
+    def play_and_pause(self):
+        self.channel_action.tap_element("play_button")
+        self.channel_action.logger.info("[Channel] Play")
+        time.sleep(1)
+        self.channel_action.tap_element("play_button")
+        self.channel_action.logger.info("[Channel] Pause")
+
+    def close_channel_inspector(self):
+        self.channel_action.tap_element("inspector_channel")
+        self.channel_action.logger.info("[Channel] Close Channel Inspector")
+
+if __name__ == "__main__":
+    channel = Channel()
+    channel.open_channel_inspector()
+    channel.open_channel_dropdown()
+    channel.select_channel_option("stereo")
+    channel.play_and_pause()
+    channel.open_channel_dropdown()
+    channel.select_channel_option("왼쪽_소리를_채우기")
+    channel.play_and_pause()
+    channel.open_channel_dropdown()
+    channel.select_channel_option("오른쪽_소리를_채우기")
+    channel.play_and_pause()
